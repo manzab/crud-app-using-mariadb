a581ac13d1f8f4dce7744d783a4b1975c2344fba
mantaszab <mantaszab@yahoo.com>
2021-01-10 22:21:59 +0200

    fixed delete, added update button,  edited styles

--- index.php
+++ index.php
@@ -13,16 +13,32 @@ if (!$conn) {
 }
 
 echo "Connected successfully";
-if(isset($_GET['action']) and $_GET['action'] == 'delete'){
+mysqli_query($conn, "SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));");
+
+if(isset($_GET['action']) and $_GET['action'] == 'deleteProject'){
     $sql = 'DELETE FROM projects WHERE id = ?';
     $stmt = $conn->prepare($sql);
     $stmt->bind_param('i', $_GET['id']);
     $res = $stmt->execute();
     $stmt->close();
     mysqli_close($conn);
-    header("Location: " . strtok($_SERVER['REQUEST_URI'], '?'));
+    header("Location: ?path=projects");
     die();
-}
+};
+if(isset($_GET['action']) and $_GET['action'] == 'deleteEmployee'){
+    $sql = 'DELETE FROM employees WHERE id = ?';
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('i', $_GET['id']);
+    $res = $stmt->execute();
+    $stmt->close();
+    mysqli_close($conn);
+    header("Location: ?path=employees");
+    die();
+};
+// if(isset($_GET['action']) and $_GET['action'] == 'updateProject') {
+
+// }
+
 print("<head><link rel='stylesheet' href='styles.css'></head>");
 print("<body>");
 print("<br>");
@@ -31,13 +47,14 @@ print("<header><div class='links'><a href='$path=projects'>PROJECTS</a><a href='
 print("</header");
 print("<br>");
 if (isset($_GET['path']) and $_GET['path'] == 'projects') {
-    $sql = "SELECT projects.id, projects.name as Projects, GROUP_CONCAT(employees.name SEPARATOR ', ') as Employees FROM projects LEFT JOIN employees ON projects.employees_id = employees.id GROUP BY projects.id";
+
+    $sql = "SELECT projects.id, GROUP_CONCAT(employees.name SEPARATOR ', ') as Employees, projects.name as Projects FROM projects LEFT JOIN employees ON projects.employees_id = employees.id GROUP BY projects.name";
     $result = mysqli_query($conn, $sql);
     if (mysqli_num_rows($result) > 0) {
         print("<table>");
         print("<thead><tr><th>#</th><th>Projects</th><th>Employees</th><th>Actions</th></tr></thead><tbody>");
         while ($row = mysqli_fetch_assoc($result)) {
-            print('<tr>' . '<td>' . $num++ . '</td>' . '<td>' . $row['Projects'] . '</td>' . '<td>' . $row['Employees'] . '</td>' . '<td>' . '<a href="?action=delete&id=' . $row['id'] . '"><button>DELETE</button></a>'. '</td>'. '</tr>');
+            print('<tr>' . '<td>' . $num++ . '</td>' . '<td>' . $row['Projects'] . '</td>' . '<td>' . $row['Employees'] . '</td>' . '<td>' . '<a href="?action=deleteProject&id=' . $row['id'] . '"><button>DELETE</button></a>'.'<a href="?action=updateProject&id=' . $row['id'] .'"><button>UPDATE</button></a>'. '</td>'. '</tr>');
         }
         print('</tbody></table>');
     } else {
@@ -45,13 +62,13 @@ if (isset($_GET['path']) and $_GET['path'] == 'projects') {
     }
 };
 if (isset($_GET['path']) and $_GET['path'] == 'employees') {
-    $sql = "SELECT employees.name as Employees, projects.name as Projects FROM employees LEFT JOIN projects ON employees.project_id = projects.id";
+    $sql = "SELECT employees.id , employees.name as Employees, projects.name as Projects FROM employees LEFT JOIN projects ON employees.project_id = projects.id";
     $result = mysqli_query($conn, $sql);
     if (mysqli_num_rows($result) > 0) {
         print("<table>");
         print("<thead><tr><th>#</th><th>Name</th><th>Projects</th><th>Actions</th></tr></thead><tbody>");
         while ($row = mysqli_fetch_assoc($result)) {
-            print("<tr>" . "<td>" . $num++ . "</td>" . "<td>" . $row["Employees"] . "</td>" . "<td>" . $row["Projects"] . "</td>" . "</tr><br>");
+            print("<tr>" . "<td>" . $num++ . "</td>" . "<td>" . $row["Employees"] . "</td>" . "<td>" . $row["Projects"] . "</td>" . '<td>' . '<a href="?action=deleteEmployee&id=' . $row['id'] . '"><button>DELETE</button></a>'. '<a href="?action=updateProject&id=' . $row['id'] .'"><button>UPDATE</button></a>'. '</td>'. '</tr>');
         }
         print("</tbody></table>");
     } else {
